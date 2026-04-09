f6834c156b86599cf34d2442e573be4d2e4590a9
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-09 15:18:59 +0200

    creato array di oggetti per snack 1

---
+++ script.js
@@ -0,0 +1,23 @@
+// seleziono il div dove mostrerò il risultato
+const risultato = document.querySelector("#risultato");
+
+// SNACK 1
+
+// funzione che parte quando clicco il bottone snack 1
+function snack1() {
+
+  // creo array di oggetti 
+  const bici = [
+    {
+      nome: "Bici 1",
+      peso: 7
+    },
+    {
+      nome: "Bici 2",
+      peso: 5
+    },
+    {
+      nome: "Bici 3",
+      peso: 8
+    }
+  ];
